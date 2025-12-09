9756b3dbd30e5304f96cc2d6edd39cfdcf8608a1
barnolacesc <francescbarnolacalls@gmail.com>
2025-12-09 23:06:01 +0100

    Configure file visibility in telescope and snacks explorer
    - Telescope (leader+ff): Show hidden files but respect .gitignore
    - Snacks explorer: Show both hidden and gitignored files by default
    - Add CLAUDE.md to .gitignore to keep it local
    - Update plugin lockfile with telescope.nvim and other dependencies

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,14 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  keys = {
+    {
+      "<leader>ff",
+      function()
+        require("telescope.builtin").find_files({
+          hidden = true,
+        })
+      end,
+      desc = "Find Files (including hidden)",
+    },
+  },
+}
